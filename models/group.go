2ed0efbcf6354858a597eaed528b46893cbec707
Kichiyaki <xyztojajestem@gmail.com>
2020-07-19 16:21:32 +0200

    the cron messages are fully translatable

--- models/group.go
+++ models/group.go
@@ -4,13 +4,15 @@ type Group struct {
 	ID                         int          `pg:",pk" json:"id" gqlgen:"id"`
 	ConqueredVillagesChannelID string       `pg:",use_zero" json:"conqueredVillagesChannelID" gqlgen:"conqueredVillagesChannelID"`
 	LostVillagesChannelID      string       `pg:",use_zero" json:"lostVillagesChannelID" gqlgen:"lostVillagesChannelID"`
-	ServerID                   string       `pg:"on_delete:CASCADE,use_zero" json:"serverID" gqlgen:"serverID"`
 	ShowEnnobledBarbarians     bool         `pg:",use_zero"`
+	ServerID                   string       `pg:"on_delete:CASCADE,use_zero" json:"serverID" gqlgen:"serverID"`
 	Server                     *Server      `json:"server,omitempty" gqlgen:"server"`
 	Observations               Observations `json:"observation,omitempty" gqlgen:"observation"`
 }
 
 type GroupFilter struct {
+	tableName struct{} `urlstruct:"group"`
+
 	ID       []int
 	ServerID []string
 	Limit    int      `urlstruct:",nowhere"`
