62c84e58c489ac4ed28935aa74b92e915640c09a
Kichiyaki <xyztojajestem@gmail.com>
2021-07-18 10:02:13 +0200

    move hndlrChangeLanguage from observations.go to admin_commands.go

--- discord/observations.go
+++ discord/observations.go
@@ -35,7 +35,6 @@ const (
 	DisableLostVillagesCommand      Command = "disablelostvillages"
 	ConqueredVillagesCommand        Command = "conqueredvillages"
 	DisableConqueredVillagesCommand Command = "disableconqueredvillages"
-	ChangeLanguageCommand           Command = "changelanguage"
 	ShowInternalsCommand            Command = "showinternals"
 )
 
@@ -895,69 +894,6 @@ func (hndlr *hndlrShowEnnobledBarbarians) execute(ctx *commandCtx, m *discordgo.
 	hndlr.SendMessage(m.ChannelID,
 		ctx.localizer.MustLocalize(&i18n.LocalizeConfig{
 			MessageID: message.ShowEnnobledBarbsSuccess2,
-			TemplateData: map[string]interface{}{
-				"Mention": m.Author.Mention(),
-			},
-		}))
-}
-
-type hndlrChangeLanguage struct {
-	*Session
-}
-
-var _ commandHandler = &hndlrChangeLanguage{}
-
-func (hndlr *hndlrChangeLanguage) cmd() Command {
-	return ChangeLanguageCommand
-}
-
-func (hndlr *hndlrChangeLanguage) requireAdmPermissions() bool {
-	return true
-}
-
-func (hndlr *hndlrChangeLanguage) execute(ctx *commandCtx, m *discordgo.MessageCreate, args ...string) {
-	argsLength := len(args)
-	if argsLength != 1 {
-		hndlr.SendMessage(m.ChannelID,
-			m.Author.Mention()+" "+ctx.localizer.MustLocalize(&i18n.LocalizeConfig{
-				MessageID: message.HelpChangageLanguage,
-				TemplateData: map[string]interface{}{
-					"Command":   hndlr.cmd().WithPrefix(hndlr.cfg.CommandPrefix),
-					"Languages": getAvailableLanguages(),
-				},
-			}))
-		return
-	}
-
-	lang := args[0]
-	valid := isValidLanguageTag(lang)
-	if !valid {
-		hndlr.SendMessage(m.ChannelID,
-			ctx.localizer.MustLocalize(&i18n.LocalizeConfig{
-				MessageID: message.ChangeLanguageLanguageNotSupported,
-				TemplateData: map[string]interface{}{
-					"Mention": m.Author.Mention(),
-				},
-			}))
-		return
-	}
-
-	ctx.server.Lang = lang
-	if err := hndlr.cfg.ServerRepository.Update(context.Background(), ctx.server); err != nil {
-		hndlr.SendMessage(m.ChannelID,
-			ctx.localizer.MustLocalize(&i18n.LocalizeConfig{
-				MessageID: message.InternalServerError,
-				TemplateData: map[string]interface{}{
-					"Mention": m.Author.Mention(),
-				},
-			}))
-		return
-	}
-	ctx.localizer = message.NewLocalizer(lang)
-
-	hndlr.SendMessage(m.ChannelID,
-		ctx.localizer.MustLocalize(&i18n.LocalizeConfig{
-			MessageID: message.ChangeLanguageSuccess,
 			TemplateData: map[string]interface{}{
 				"Mention": m.Author.Mention(),
 			},
