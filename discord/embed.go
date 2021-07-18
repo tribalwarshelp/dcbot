1833d3893de2b0c2a37a3e52243e3d01e221b6e9
Kichiyaki <xyztojajestem@gmail.com>
2021-07-18 12:26:07 +0200

    remove unnecessary +1 in splitEmbedFields

--- discord/embed.go
+++ discord/embed.go
@@ -304,7 +304,7 @@ func splitEmbedFields(e *Embed) [][]*discordgo.MessageEmbedField {
 		fNameLen := len(field.Name)
 		fValLen := len(field.Value)
 		if characters+fNameLen+fValLen > EmbedSizeLimit || index == fieldsLen-1 {
-			splitFields = append(splitFields, fields[fromIndex:index+1])
+			splitFields = append(splitFields, fields[fromIndex:index])
 			fromIndex = index
 			characters = baseNumberOfCharacters
 		}
